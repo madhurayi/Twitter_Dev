be09da2c535642542a30f7e1498e6f76bc8c4e40
madhu <rayi.madhupriya@gmail.com>
2024-03-21 23:35:52 +0530

    Updated comment model with refpath

--- src/models/comment.js
+++ src/models/comment.js
@@ -4,9 +4,21 @@ const commentSchema= new mongoose.Schema({
         type: String,
         required: true
     },
-    userEmail:{
-        type: String
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
+    onModel:{
+        type: String,
+        required: true,
+        enum:['Tweet','Comment']
+    },
+    commentable:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'onModel' ,
+        required: true
+    }
 },{timestamps: true});
 
 const Comment= mongoose.model('Comment',commentSchema);
